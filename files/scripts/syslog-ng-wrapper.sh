3180e412273ade7ecceac7d8fbf8181745da69a5
Stephen Garlick <sgarlick@taximagic.com>
2015-01-30 12:20:44 -0500

    make token env varible

--- files/scripts/syslog-ng-wrapper.sh
+++ files/scripts/syslog-ng-wrapper.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-TOKEN=e8bcd155-264b-4ec0-88be-fcb023f76a89
+
+if [ -z "$TOKEN" ]; then
+  echo "Missing \$TOKEN"
+  exit 1
+fi
+
 TAG=$(curl --connect-timeout 3 http://169.254.169.254/latest/meta-data/instance-id)
 
 sed -i "s/TOKEN/$TOKEN/" /etc/syslog-ng/conf.d/22-loggly.conf
 sed -i "s/TAG/$TAG/" /etc/syslog-ng/conf.d/22-loggly.conf
 
-exec /usr/sbin/syslog-ng -F 
+exec /usr/sbin/syslog-ng -F --no-caps
